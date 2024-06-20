a45bdfb6084e9f41003995236fc7a8afbf750fdf
Vitalik Kravets <vitalikkravez1@gmail.com>
2024-06-20 15:29:08 +0300

    remove unessery files

--- api/app.js
+++ api/app.js
@@ -6,15 +6,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var qrsRouter = require('./routes/qr');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(cors())
@@ -22,8 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/qrs', qrsRouter);
 
 // catch 404 and forward to error handler
